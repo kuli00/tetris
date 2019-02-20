f131457e6036add195e443904b20f1baa1a819ec
Maciej Kuliński <kulinskimm@gmail.com>
2019-02-20 22:52:24 +0100

    Figures falling down

---
+++ js/figures.js
@@ -0,0 +1,53 @@
+var figure1 = [
+    [-4, null, 3, null, null],
+    [-3, null, 3, null, null],
+    [-2, null, 3, null, null],
+    [-1, null, 3, null, null],
+];
+
+var figure2 = [
+    [-4, null, null, null, null],
+    [-3, null, 3, null, null],
+    [-2, null, 3, 4, null],
+    [-1, null, null, 4, null],
+];
+var figure3 = [
+    [-4, null, null, null, null],
+    [-3, null, null, 4, null],
+    [-2, null, 3, 4, null],
+    [-1, null, 3, null, null],
+];
+var figure4 = [
+    [-4, null, null, null, null],
+    [-3, null, null, null, null],
+    [-2, null, 3, null, null],
+    [-1, null, 3, 4, 5],
+];
+var figure5 = [
+    [-4, null, null, null, null],
+    [-3, null, null, null, null],
+    [-2, null, null, null, 5],
+    [-1, null, 3, 4, 5],
+];
+var figure6 = [
+    [-4, null, null, null, null],
+    [-3, null, null, null, null],
+    [-2, null, null, 4, null],
+    [-1, null, 3, 4, 5],
+];
+var figure7 = [
+    [-4, null, null, null, null],
+    [-3, null, null, null, null],
+    [-2, null, 3, 4, null],
+    [-1, null, 3, 4, null],
+];
+
+var figures = [
+    figure1,
+    figure2,
+    figure3,
+    figure4,
+    figure5,
+    figure6,
+    figure7,
+];
--- js/game.js
+++ js/game.js
@@ -6,6 +6,13 @@ var startTime;
 var seconds = 0;
 var minutes = 0;
 
+var colors = [
+    "#ff0000",
+    "#00ff00",
+    "#0000ff",
+    "#00ffff",
+];
+
 function generateGameBoard() {
     $("#game").empty();
     for (var i = 0; i < 12; i++) {
@@ -15,10 +22,8 @@ function generateGameBoard() {
         for (var j = 0; j < 8; j++) {
             let newDiv = document.createElement("DIV");
             newDiv.className = "game-field";
-            const fieldId = i + "," + j;
+            const fieldId = i + "_" + j;
             newDiv.id = fieldId;
-            newDiv.height = "30px";// = newDiv.width;
-            //newDiv.textContent = "a";
             newRow.append(newDiv);
 
         }
@@ -29,6 +34,7 @@ function generateGameBoard() {
 function startGame() {
     generateGameBoard();
     timerStart();
+    game();
 }
 
 function timerStart() {
@@ -74,4 +80,103 @@ function timerUpdate() {
 function parseTimeUnits() {
     seconds = parseInt(seconds);
     minutes = parseInt(minutes);
+}
+
+function pickNewFigure() {
+    let figureNumber = Math.floor(Math.random() * figures.length);
+    return figures[figureNumber];
+}
+
+function pickNewColor(oldColor) {
+    let colorId = Math.floor(Math.random() * colors.length);
+    if (oldColor === null) {
+        return colors[colorId];
+    }
+    if (colors[colorId] === oldColor) {
+        pickNewColor(oldColor);
+    }
+    return colors[colorId];
+}
+
+function game() {
+    let oldColor = null;
+    let newFigure = true;
+    let nextFigure = new Array();
+    let currentFigurePlaced = true;
+
+    let color;
+    setInterval(function () {
+        let currentFigurePosition = new Array();
+        if (currentFigurePlaced) {
+            nextFigure = pickNewFigure();
+            color = pickNewColor(oldColor);
+            currentFigurePlaced = false;
+        }
+        currentFigurePlaced = moveFigureDown(nextFigure, color);
+    }, 500);
+}
+
+
+function moveFigureDown(nextFigure, color) {
+    clearOldFigure(nextFigure);
+    for (let i = 3; i >=0; i--) {
+        nextFigure[i][0] += 1;
+    }
+    if(checkConflict(nextFigure) === false) {
+        return true;
+    }
+    printNewFigure(nextFigure, color);
+    refreshGameBoard();
+    return false;
+}
+
+function clearOldFigure(oldFigure) {
+    for (let i = 0; i <=3; i++) {
+        for (let j = 1; j <= 4; j++) {
+            if (oldFigure[i][j] !== null) {
+                var y = oldFigure[i][0];
+                var x = oldFigure[i][j];
+                if (y >= 0) {
+                    gameBoard[y][x] = 0;
+                }
+            }
+        }
+    }
+}
+
+function printNewFigure(nextFigure, color) {
+    for (let i = 0; i <= 3; i++) {
+        for (let j = 1; j <= 4; j++) {
+            if (nextFigure[i][j] !== null) {
+                var x = nextFigure[i][j];
+                var y = nextFigure[i][0];
+                if (y >= 0) {
+                    gameBoard[y][x] = color;
+                }
+            }
+        }
+    }
+}
+
+function refreshGameBoard() {
+    for (let i = 0; i <= 11; i++){
+        for (let j = 0; j <= 7; j++) {
+            var fieldId = "#" + i + "_" + j;
+            var color = gameBoard[i][j];
+            if (color === 0) {
+                color = "#ffff00";
+            }
+            $(fieldId).css("background-color", color);
+        }
+    }
+}
+
+function checkConflict(nextFigure) {
+    for (let i = 3; i >= 0; i--){
+        for (let j = 0; j <= 3; j++) {
+            if (nextFigure[i][0] > gameBoard.length - 1) {
+                return false;
+            }
+        }
+    }
 }
